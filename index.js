ddd68f4a0a6a1feb6e44cc3476516621fe62fc85
Amine Mesbahi <mesbahiamine886@gmail.com>
2022-01-27 13:52:12 +0100

    start web resturant

---
+++ index.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('.baremenu'),
+menuB = document.querySelector('.menu'),
+list = document.querySelector('.menu-list');
+
+menu.addEventListener('click',() =>{
+    menuB.classList.toggle('active');
+    
+});
